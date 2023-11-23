1851cccf60654956af77cd91e27ca146b5a263a2
ccrsxx <aminrisal@gmail.com>
2023-11-23 23:44:03 +0700

    feat: add tailwind colors from figma design

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import colors from 'tailwindcss/colors';
 import type { Config } from 'tailwindcss';
 
 const config: Config = {
@@ -10,11 +9,33 @@ const config: Config = {
   theme: {
     extend: {
       colors: {
-        accent: {
-          main: colors.pink[400],
-          start: colors.purple[500],
-          end: colors.pink[400]
+        primary: {
+          blue: {
+            100: '#E2D4F0',
+            200: '#D0B7E6',
+            300: '#489CFF',
+            400: '#6148FF57',
+            500: '#6148FF'
+          },
+          cream: {
+            100: '#FFF8ED',
+            200: '#FFF0DC',
+            300: '#FFE9CA',
+            400: '#D4C2A8',
+            500: '#AA9B87'
+          },
+          neutral: {
+            100: '#FFFFFF',
+            200: '#D0D0D0',
+            300: '#8A8A8A',
+            400: '#3C3C3C',
+            500: '#151515'
+          }
         }
+      },
+      boxShadow: {
+        low: '0 0 4px',
+        high: '0 0 10px'
       }
     }
   },
