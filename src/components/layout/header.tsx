8bd2c3cfe446fa7ed5847bd7a3c94b2a8f7ff3bd
ccrsxx <aminrisal@gmail.com>
2023-11-25 18:43:12 +0700

    feat: make header login button has clickable style

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -5,11 +5,11 @@ import { Button } from '../ui/button';
 
 export function Header(): JSX.Element {
   return (
-    <header className='flex justify-center bg-primary-blue-500'>
+    <header className='bg-primary-blue-500'>
       <div className='layout flex w-full items-center gap-8 p-4'>
         <Logo />
         <SearchBar />
-        <Button className='ml-auto flex items-center gap-2 text-xl font-bold'>
+        <Button className='clickable ml-auto flex items-center gap-2 text-xl font-bold'>
           <MdLogin />
           Masuk
         </Button>
