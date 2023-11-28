f946303b88fc038f4a9236d8b8c8bbdc8749e490
ccrsxx <aminrisal@gmail.com>
2023-11-28 14:07:55 +0700

    feat: add opacity animation header login / logout button

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import Link from 'next/link';
+import { clsx } from 'clsx';
 import { MdLogin, MdLogout } from 'react-icons/md';
 import { useAuth } from '@/lib/context/auth-context';
 import { Logo } from '../common/logo';
@@ -15,23 +16,30 @@ export function Header(): JSX.Element {
       <div className='layout flex w-full items-center gap-8 p-4'>
         <Logo />
         <SearchBar />
-        {loading ? null : user ? (
-          <Button
-            className='clickable ml-auto flex items-center gap-2 text-xl font-bold'
-            onClick={logout}
-          >
-            <MdLogout />
-            Log out
-          </Button>
-        ) : (
-          <Link
-            href='/login'
-            className='clickable ml-auto flex items-center gap-2 text-xl font-bold'
-          >
-            <MdLogin />
-            Masuk
-          </Link>
-        )}
+        <div
+          className={clsx(
+            'ml-auto opacity-0 transition-opacity',
+            !loading && 'opacity-100'
+          )}
+        >
+          {loading ? null : user ? (
+            <Button
+              className='clickable flex items-center gap-2 text-xl font-bold'
+              onClick={logout}
+            >
+              <MdLogout />
+              Log out
+            </Button>
+          ) : (
+            <Link
+              href='/login'
+              className='clickable flex items-center gap-2 text-xl font-bold'
+            >
+              <MdLogin />
+              Masuk
+            </Link>
+          )}
+        </div>
       </div>
     </header>
   );
