408f98461221d9d3ce2360c99617e40772246618
ccrsxx <aminrisal@gmail.com>
2023-12-05 21:09:53 +0700

    feat: improve video player

--- src/components/ui/video-player.tsx
+++ src/components/ui/video-player.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link';
 import { Button } from './button';
 
 type VideoPlayerProps = {
@@ -12,15 +13,21 @@ export function VideoPlayer({
   const youtubeId = src.split('/').pop();
 
   return (
-    <div className='relative h-80 rounded-xl bg-black'>
+    <div className='group relative h-80 rounded-xl bg-black'>
       <iframe
         className='h-full w-full rounded-xl'
         src={`https://www.youtube.com/embed/${youtubeId}`}
       />
-      <div className='absolute bottom-0  right-0 flex gap-3 p-4'>
-        <Button className='clickable bg-primary-blue-50 px-4 py-2 text-primary-blue-300'>
+      <div
+        className='absolute bottom-0 right-0 flex gap-3 p-4 
+                   opacity-0 transition-opacity group-hover:opacity-100'
+      >
+        <Link
+          href='/courses'
+          className='clickable bg-primary-blue-50 px-4 py-2 text-primary-blue-300'
+        >
           Kelas lainnya
-        </Button>
+        </Link>
         <Button
           className='clickable bg-primary-blue-500 px-4 py-2 text-white'
           onClick={handleNextMaterial}
