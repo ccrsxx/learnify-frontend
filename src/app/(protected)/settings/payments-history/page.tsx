71a6b062f7d9ae0509aec73a099aa25921f3669a
ccrsxx <aminrisal@gmail.com>
2023-12-26 18:52:24 +0700

    feat: improve layout payments history

--- src/app/(protected)/settings/payments-history/page.tsx
+++ src/app/(protected)/settings/payments-history/page.tsx
@@ -10,28 +10,26 @@ export default function PaymentsHistory(): JSX.Element {
   const paymentsHistory = data?.data;
 
   return (
-    <section className='mx-auto w-full max-w-md rounded-medium p-6 shadow-low'>
-      <section className='grid gap-4'>
-        {isLoading ? (
-          Array.from({ length: 3 }).map((_, i) => (
-            <CourseCardSkeleton key={i} />
-          ))
-        ) : paymentsHistory?.length ? (
-          <>
-            {data?.data.map((payment) => (
-              <CourseCard
-                course={payment.course}
-                payment={payment}
-                key={payment.id}
-              />
-            ))}
-          </>
-        ) : (
-          <p className='mx-auto max-w-md p-4 font-medium text-black'>
+    <section className='mx-auto grid w-full max-w-md gap-4 rounded-medium p-6 shadow-low'>
+      {isLoading ? (
+        Array.from({ length: 3 }).map((_, i) => <CourseCardSkeleton key={i} />)
+      ) : paymentsHistory?.length ? (
+        <>
+          {data?.data.map((payment) => (
+            <CourseCard
+              course={payment.course}
+              payment={payment}
+              key={payment.id}
+            />
+          ))}
+        </>
+      ) : (
+        <section className='flex justify-center'>
+          <h1 className='max-w-md p-4 font-medium text-black'>
             Tidak ada riwayat pembayaran
-          </p>
-        )}
-      </section>
+          </h1>
+        </section>
+      )}
     </section>
   );
 }
