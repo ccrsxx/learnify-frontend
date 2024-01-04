491c8be24474909e9893bb6ef7272699decf96c4
ccrsxx <aminrisal@gmail.com>
2024-01-04 11:17:49 +0700

    style: improve classname formatting

--- src/components/modal/image-preview.tsx
+++ src/components/modal/image-preview.tsx
@@ -63,11 +63,11 @@ export function ImagePreview({
                   alt={alt}
                 />
                 <a
-                  className='absolute bottom-0 right-0 mx-2 mb-2 translate-y-4 rounded-low bg-white/40 
-                         px-2 py-1  text-sm text-black/80 opacity-0 outline-none transition hover:!bg-primary-blue-500 
-                         hover:text-white focus-visible:translate-y-0 focus-visible:!bg-primary-blue-500
-                         focus-visible:text-white focus-visible:opacity-100 group-hover:translate-y-0 
-                         group-hover:opacity-100 dark:bg-black/40 dark:text-white/80'
+                  className='absolute bottom-0 right-0 mx-2 mb-2 translate-y-4 rounded-low bg-white/40
+                             px-2 py-1  text-sm text-black/80 opacity-0 outline-none transition hover:!bg-primary-blue-500 
+                             hover:text-white focus-visible:translate-y-0 focus-visible:!bg-primary-blue-500
+                             focus-visible:text-white focus-visible:opacity-100 group-hover:translate-y-0 
+                             group-hover:opacity-100 dark:bg-black/40 dark:text-white/80'
                   href={customLink}
                   target='_blank'
                   rel='noreferrer'
@@ -78,9 +78,9 @@ export function ImagePreview({
               </div>
               <a
                 className='absolute -bottom-7 left-0 font-medium text-black/80 underline decoration-transparent 
-                       underline-offset-2 outline-none transition-colors hover:text-black hover:decoration-black
-                       focus-visible:text-black focus-visible:decoration-inherit dark:text-white/80 
-                       dark:hover:text-white dark:hover:decoration-white dark:focus-visible:text-white'
+                           underline-offset-2 outline-none transition-colors hover:text-black hover:decoration-black
+                           focus-visible:text-black focus-visible:decoration-inherit dark:text-white/80 
+                           dark:hover:text-white dark:hover:decoration-white dark:focus-visible:text-white'
                 href={customLink}
                 target='_blank'
                 rel='noreferrer'
